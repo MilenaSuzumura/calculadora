80f446d1e6a7c0a55287b5239a3d8d751381aaaf
milenasuzumura <milena.suzumura11@gmail.com>
2024-04-29 17:12:47 -0300

    placed the base item and I'm decorating it

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b940b51b99b9b0ec")
+/******/ 		__webpack_require__.h = () => ("7c5598a01addfabc")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
---
+++ src/components/View.tsx
@@ -0,0 +1,11 @@
+export default function View() {
+    return (
+        <div id="view">
+            <p>0</p>
+            <section>
+                <p>=</p>
+                <p>0</p>
+            </section>
+        </div>
+    )
+}
